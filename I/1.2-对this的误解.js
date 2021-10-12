d7edf618b03caa4ea382f8b37ad5987eeae44c8b
Lex <zhang13pro@qq.com>
2021-10-12 09:54:49 +0800

    fix: this错误用法 （LHS查询）

--- I/1.2-对this的误解.js
+++ I/1.2-对this的误解.js
@@ -9,5 +9,8 @@ function bar() {
   console.log(this.a);
 }
 
-// TypeError: this.bar is not a function
-foo(); // 浏览器环境 undefined
+/**
+ * ===> TypeError: this.bar is not a function
+ * 浏览器环境下 ===> undefined (此时引擎为变量进行LHS查询 会创建全局变量 a)
+ */
+foo();
