0a24bbfffb75e7fca7065fa2c0144f148713fe9c
zhang13pro <lexzhang13@qq.com>
2021-10-24 12:22:47 +0800

    this

--- III/2.6-this.js
+++ III/2.6-this.js
@@ -1,8 +1,14 @@
 function foo() {
+  "use strict";
   console.log(this.bar);
 }
-var bar = "global";
+let bar = "global";
 
-// 这才对嘛 书中（下卷P45）输出“global”是什么鬼
-foo(); // undefined
+/**
+ * 变量提升
+ * TDZ
+ * RHS
+ * 严格模式
+ */
+foo(); // undefined || global || TypeError
 new foo(); // undefined
